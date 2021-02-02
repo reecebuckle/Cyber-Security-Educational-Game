f35e7d91775278a23e36fd5846b4bdc20d2005fa
Reece Buckle <rtb1c17@soton.ac.uk>
2021-02-02 21:47:41 +0000

    Lobby UI
    Lobby Match making UI

---
+++ Unity Files/Assets/Scripts/TestConnect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class TestConnect : MonoBehaviourPunCallbacks 
+{
+    private void Start() {
+
+        Debug.Log("connecting to server");
+        //use server settings 
+        PhotonNetwork.NickName = "RB";
+        PhotonNetwork.GameVersion = "0.1";
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Disconnected from server: " + cause.ToString());
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("connected to server");
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName);
+
+    }
+}
