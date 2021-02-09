f8ef50affaa9956188700c28dd123de7e1d22b93
Reece Buckle <rtb1c17@soton.ac.uk>
2021-02-09 13:35:01 +0000

    BFS for Movement
    Adds BFS to find tiles, and then move towards them

---
+++ Unity Files/Assets/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Movement
+{
+    public class PlayerMovementController : MovementRules //extends movement rules
+    {
+        private void Start() => Init();
+
+        private void Update()
+        {
+            if (!moving)
+            {
+                FindSelectableTiles();
+                CheckMouseClick();
+            }
+            else
+            {
+                Move();
+            }
+        }
+
+
+        private void CheckMouseClick()
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.tag == "Tile")
+                    {
+                        Tile t = hit.collider.GetComponent<Tile>();
+
+                        if (t.selectedTile)
+                        {
+                            MoveToTile(t);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Unity Files/Assets/Scripts/Movement/Tile.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Guide, No Jump Height Implemented, refer to this if we want to integrate jump height
+ * https://www.youtube.com/watch?v=cK2wzBCh9cg&t=1554s&ab_channel=GameProgrammingAcademy
+ */
+
+namespace Movement
+{
+    public class Tile : MonoBehaviour
+    {
+        [Header("Boolean Variables")]
+        public bool walkable = true; //is the tile walkable? Cannot step on opposing players!
+
+        public bool currentTile = false;
+        public bool targetTile = false;
+        public bool selectedTile = false;
+
+        [Header("BFS Variables")] public List<Tile> adjacencyList = new List<Tile>();
+        public bool visited = false;
+        public Tile parent = null;
+        public int distance = 0; //set default to 5
+
+
+        private void Update() => SelectTile();
+
+        /*
+         * Highlights the selected tile
+         */
+        private void SelectTile()
+        {
+            if (currentTile)
+                GetComponent<Renderer>().material.color = Color.magenta;
+            else if (targetTile)
+                GetComponent<Renderer>().material.color = Color.green;
+            else if (selectedTile)
+                GetComponent<Renderer>().material.color = Color.red;
+            else
+                GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        /*
+         * 
+         */
+        public void FindNeighbours()
+        {
+            Reset();
+
+            //Check the 4 directions (forwards, backwards, right and left)
+            CheckTile(Vector3.forward);
+            CheckTile(-Vector3.forward);
+            CheckTile(Vector3.right);
+            CheckTile(-Vector3.right);
+        }
+
+        /*
+         * Checks adjacent tiles by observing their colliders with a raycast.
+         * If it's a valid tile to move to, adds it to an adjacency list for BFS.
+         */
+        private void CheckTile(Vector3 direction)
+        {
+            Vector3 halfExtents = new Vector3(0.25f, 0.25f, 0.25f);
+            //returns an array of colliders
+            Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtents);
+
+            //iterate through colliders, check if there's a tile present
+            foreach (Collider item in colliders)
+            {
+                Tile tile = item.GetComponent<Tile>();
+
+                //if the tile is null or not walkable, move onto the next collider..
+                if (tile != null && tile.walkable)
+                {
+                    RaycastHit hit;
+
+                    //from centre of tile, look to see if something is there, if it is, add it to our adjacency list
+
+                    if (!Physics.Raycast(tile.transform.position, Vector3.up, out hit, 1))
+                        adjacencyList.Add(tile);
+                }
+            }
+        }
+
+        /*
+         * Reset all variables after every turn
+         */
+        public void Reset()
+        {
+            adjacencyList.Clear();
+
+            currentTile = false;
+            targetTile = false;
+            selectedTile = false;
+
+            visited = false;
+            parent = null;
+            distance = 0;
+        }
+    }
+}
