574c46f5f52d8a0bea1b11882265689bee6206c2
Reece Buckle <rtb1c17@soton.ac.uk>
2021-03-07 18:22:44 +0000

    UI+Units
    Updating UI/Units

--- TurnBasedProjectV2/Assets/Scripts/Unit.cs
+++ TurnBasedProjectV2/Assets/Scripts/Unit.cs
@@ -6,14 +6,28 @@ namespace Units
 {
     public class Unit : MonoBehaviourPun
     {
-        [Header("Unit Properties")] [SerializeField]
-        private float moveSpeed; // units per second when moving
-
+        //TODO Convert these into public variables?
+        [Header("Unit Properties")] 
+        
+        [SerializeField] private float moveSpeed; // units movement speed
         [SerializeField] private int moveDistance; // max distance we can move per turn
-
-        [SerializeField] private int attackDistance; // max distance we can attack
-        private bool hasMovedThisTurn; // has this unit been used this turn?
+        [SerializeField] private int maxHP; // maximum health points a unit has
+        [SerializeField] private int maxDefence; // current defence points a unit has
+        
+        private int currentHP; // current hit points a unit has
+        private int currentDef; // current defence a unit has
+        
+        private bool hasMovedThisTurn; 
         private bool isSelected;
+
+        /*
+         * Initiate units current health and defence (which are variables
+         */
+        private void Start()
+        {
+            currentHP = maxHP;
+            currentDef = maxDefence;
+        }
         
         // called when the unit is spawned in
         [PunRPC]
@@ -33,6 +47,26 @@ namespace Units
         */
         public void ToggleMovedThisTurn(bool hasMoved) => hasMovedThisTurn = hasMoved;
         
+        
+        
+        /*
+         * Invoked when another unit attacks this unit, and instructs this unit to take damage
+         * TODO: Incorporate defence into this maybe??
+         */
+        [PunRPC]
+        private void TakeDamage (int damage)
+        {
+            currentHP -= damage;
+
+            if (currentHP <= 0)
+                photonView.RPC("UnitHasDied", RpcTarget.All);
+            else
+            {
+                // update health UI
+                photonView.RPC("UpdateHealthBar", RpcTarget.All, (float) currentHP / (float) maxHP);
+            }
+        }
+
         // Getter method to see if we can use this unit
         public bool MovedThisTurn()
         {
@@ -58,149 +92,12 @@ namespace Units
         }
 
         //Getter method for attack distance
-        public int GetAttackDistance()
+        /*public int GetAttackDistance()
         {
             return attackDistance;
-        }
+        }*/
 
        
         
-
-
-        /*public int curHp;               // current health
-    public int maxHp;               // maximum health
-    public float moveSpeed;         // units per second when moving
-    public int minDamage;           // minimum damage
-    public int maxDamage;           // maximum damage
-
-    public int maxMoveDistance;     // max distance we can move per turn
-    public int maxAttackDistance;   // max distance we can attack
-
-    public bool usedThisTurn;       // has this unit been used this turn?
-
-    public GameObject selectedVisual;   // selection circle sprite
-    public SpriteRenderer spriteVisual; // sprite of the unit
-
-    [Header("UI")]
-    public Image healthFillImage;       // health bar fill image
-
-    [Header("Sprite Variants")]
-    public Sprite leftPlayerSprite;     // left player sprite (blue)
-    public Sprite rightPlayerSprite;    // right player sprite (red)
-
-    // called when the unit is spawned in
-    [PunRPC]
-    void Initialize (bool isMine)
-    {
-        if(isMine) PlayerController.me.units.Add(this);
-        else GameManager.instance.GetOtherPlayer(PlayerController.me).units.Add(this);
-
-        healthFillImage.fillAmount = 1.0f;
-
-        // set sprite variant
-        spriteVisual.sprite = transform.position.x < 0 ? leftPlayerSprite : rightPlayerSprite;
-
-        // rotate the unit
-        spriteVisual.transform.up = transform.position.x < 0 ? Vector3.left : Vector3.right;
-    }
-
-    // can we be selected?
-    public bool CanSelect ()
-    {
-        if(usedThisTurn) return false;
-        else return true;
-    }
-
-    // can we move to this position?
-    public bool CanMove (Vector3 movePos)
-    {
-        if(Vector3.Distance(transform.position, movePos) <= maxMoveDistance)
-            return true;
-        else return false;
-    }
-
-    // can we attack this position?
-    public bool CanAttack (Vector3 attackPos)
-    {
-        if(Vector3.Distance(transform.position, attackPos) <= maxAttackDistance)
-            return true;
-        else return false;
-    }
-
-    /*
-     * Invoked when a unit is selected
-     #1#
-    public void ToggleSelect (bool selected) => selectedVisual.SetActive(selected);
-    
-
-    public void Move (Vector3 targetPos)
-    {
-        usedThisTurn = true;
-
-        // rotate sprite
-        Vector3 dir = (transform.position - targetPos).normalized;
-        spriteVisual.transform.up = dir;
-
-        StartCoroutine(MoveOverTime());
-
-        IEnumerator MoveOverTime ()
-        {
-            while(transform.position != targetPos)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-                yield return null;
-            }
-        }
-    }
-
-    // attacks the requested enemy unit
-    public void Attack (Unit unitToAttack)
-    {
-        usedThisTurn = true;
-        unitToAttack.photonView.RPC("TakeDamage", PlayerController.enemy.photonPlayer, Random.Range(minDamage, maxDamage + 1));
-    }
-
-    /*
-     * Invoked when attatcked by another unit
-     #1#
-    [PunRPC]
-    void TakeDamage (int damage)
-    {
-        curHp -= damage;
-
-        if (curHp <= 0)
-            photonView.RPC("Die", RpcTarget.All);
-        else
-        {
-            // update health UI
-            photonView.RPC("UpdateHealthBar", RpcTarget.All, (float)curHp / (float)maxHp);
-        }
-    }
-
-    /*
-     * Updates UI Health Bar
-     #1#
-    [PunRPC]
-    void UpdateHealthBar (float fillAmount) => healthFillImage.fillAmount = fillAmount;
-    
-    /*
-     * Invoked when unit's HP reaches 0
-     #1#
-    [PunRPC]
-    void Die ()
-    {
-        if(!photonView.IsMine)
-            PlayerController.enemy.units.Remove(this);
-        else
-        {
-            PlayerController.me.units.Remove(this);
-
-            // check the win condition
-            GameManager.instance.CheckWinCondition();
-
-            // destroy the unit across the network
-            PhotonNetwork.Destroy(gameObject);
-        }
-    }*/
     }
 }
